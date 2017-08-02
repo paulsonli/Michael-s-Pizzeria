a8046717b7babd3a6655dc3d9797a05f14839400
Paulson Li <paulsonli99@gmail.com>
2017-08-02 15:59:53 +0000

    gallery and homepage

--- script.js
+++ script.js
@@ -2,11 +2,34 @@
 /* global $ */ 
 $(document).ready(function(){
     $("#myCarousel").carousel({
-        interval: 1600        
+        interval: 1700        
     });
  
  
  
+ // Gallery
+    $('ul li').click(function(){ 
+        
+    var clicked_href = $(this).children('a').attr('href');
+    $("li").removeClass("active");
+    $(this).addClass("active"); 
+  
+    $('.carousel-inner').hide();
+    $('#ssBackground').hide();
+    
+    $('about').hide();
+    
+    
+    
+    $(clicked_href).show();
+     
+  });
+ 
+ 
+
+
+
+
  
  
 });
